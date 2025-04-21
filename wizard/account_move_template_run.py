c43f0414332eb98f86ef6657a31bf37cc6681af5
robertoCabreraAgro <roberto.cabrera@agromarin.mx>
2025-04-21 16:10:01 -0600

    update

--- wizard/account_move_template_run.py
+++ wizard/account_move_template_run.py
@@ -14,14 +14,12 @@ class AccountMoveTemplateRun(models.TransientModel):
     _name = "account.move.template.run"
     _description = "Wizard to generate move from template"
 
-    company_id = fields.Many2one(
-        comodel_name="res.company",
-        required=True,
-        default=lambda self: self.env.company,
-    )
     template_id = fields.Many2one(
         comodel_name="account.move.template",
         required=True,
+    )
+    move_type = fields.Selection(
+        related="template_id.move_type",
     )
     partner_id = fields.Many2one(
         comodel_name="res.partner",
@@ -39,9 +37,25 @@ class AccountMoveTemplateRun(models.TransientModel):
              'L2': {'partner_id': 2, 'amount': 200, 'name': 'some label 2'}, }
              """
     )
+    quantity = fields.Float(
+        string="Quantity",
+        digits="Product Unit of Measure",
+        default=False,
+    )
     price_unit = fields.Float(
         string="Unit Price",
-        default=0.0,
+        digits="Product Price",
+        default=False,
+    )
+    discount = fields.Float(
+        string="Discount (%)",
+        digits="Discount",
+        default=False,
+    )
+    balance = fields.Float(
+        string="Unit Price",
+        digits="Product Price",
+        default=False,
     )
     ref = fields.Char(string="Reference")
     line_ids = fields.One2many(
@@ -52,38 +66,38 @@ class AccountMoveTemplateRun(models.TransientModel):
 
     @api.onchange("template_id")
     def _onchange_template_id(self):
-        if not self.template_id:
-            return
-
-        self.load_lines()
+        if self.template_id:
+            self.load_lines()
 
     def load_lines(self):
         self.ensure_one()
-        self.line_ids.unlink()
-        new_line_ids = self.env["account.move.template.line.run"]
-        for tmpl_line in self.template_id.line_ids.filtered(
-            lambda line: line.type == "input"
-        ):
-            new_line_ids += self.env["account.move.template.line.run"].new(
-                self._prepare_wizard_line(tmpl_line)
+        lines = [
+            (0, 0, self._prepare_wizard_line(tmpl_line))
+            for tmpl_line in self.template_id.line_ids.filtered(
+                lambda line: line.type == "input"
             )
-        self.line_ids = new_line_ids
+        ]
+        self.line_ids = [(5, 0, 0)] + lines
+
+    def _hook_create_move(self, move_vals):
+        move = 1
+        return move
 
     def create_move(self):
         self.ensure_one()
-        company_cur = self.company_id.currency_id
-        move_vals = self._prepare_move()
+        move_vals = self._prepare_move_vals()
         for line in self.line_ids:
-            amount = line.amount
-            if not company_cur.is_zero(amount):
-                move_vals["line_ids"].append(
-                    Command.create(self._prepare_move_line(line, amount))
-                )
+            move_vals["line_ids"].append(
+                Command.create(self._prepare_move_line_vals(line))
+            )
 
-        move = self.env["account.move"].create(move_vals)
-        if hasattr(self, "price_unit") and self.price_unit:
-            for move_line in move.line_ids:
-                move_line.price_unit = self.price_unit
+        move = (
+            self.env["account.move"]
+            .with_context(default_move_type=self.move_type)
+            .create(move_vals)
+        )
+        for line in move.invoice_line_ids:
+            line._compute_tax_ids()
 
         result = self.env["ir.actions.actions"]._for_xml_id(
             "account.action_move_journal_line"
@@ -100,40 +114,37 @@ class AccountMoveTemplateRun(models.TransientModel):
         )
         return result
 
-    def _prepare_move(self):
+    def _prepare_move_line_vals(self, line):
+        vals = {
+            "name": line.name,
+            "account_id": line.account_id.id,
+            "analytic_distribution": line.analytic_distribution,
+        }
+        if self.template_id.move_type == "entry":
+            vals["balance"] = self.balance or line.balance
+
+        elif self.template_id.move_type != "entry":
+            vals["product_id"] = line.product_id.id
+            vals["quantity"] = self.quantity or line.quantity
+            vals["price_unit"] = self.price_unit or line.price_unit
+            vals["discount"] = self.discount or line.discount
+
+        return vals
+
+    def _prepare_move_vals(self):
         journal = self.env["account.journal"].search(
             [("code", "=", self.template_id.journal_code)], limit=1
         )
-        move_vals = {
-            "company_id": self.company_id.id,
+        vals = {
             "journal_id": journal.id,
             "move_type": self.template_id.move_type,
             "partner_id": self.partner_id.id or False,
+            "invoice_payment_term_id": self.template_id.invoice_payment_term_id.id or False,
             "date": self.date,
             "ref": self.ref,
             "line_ids": [],
         }
-        return move_vals
-
-    def _prepare_move_line(self, line, amount):
-
-        values = {
-            "name": line.name,
-            "account_id": line.account_id.id,
-            "balance": line.balance,
-            "partner_id": line.partner_id.id or self.partner_id.id,
-        }
-
-        if line.price_unit:
-            values["price_unit"] = line.price_unit
-
-        if line.tax_ids:
-            values["tax_ids"] = [Command.set(line.tax_ids.ids)]
-
-        if line.analytic_distribution:
-            values["analytic_distribution"] = line.analytic_distribution
-
-        return values
+        return vals
 
     def _prepare_wizard_line(self, tmpl_line):
         account_id = self.env["account.account"].search(
@@ -144,12 +155,14 @@ class AccountMoveTemplateRun(models.TransientModel):
             "name": tmpl_line.name,
             "sequence": tmpl_line.sequence,
             "partner_id": tmpl_line.partner_id.id or False,
-            "product_id": tmpl_line.product_id.id,
-            "price_unit": tmpl_line.price_unit or False,
-            "amount": 0.0,
             "account_id": account_id.id,
             "analytic_distribution": tmpl_line.analytic_distribution or False,
-            "tax_ids": [Command.set(tmpl_line.tax_ids.ids)],
+            "product_id": tmpl_line.product_id.id or False,
+            "product_uom_id": tmpl_line.product_uom_id.id or False,
+            "quantity": tmpl_line.quantity or False,
+            "price_unit": tmpl_line.price_unit or False,
+            "discount": tmpl_line.discount or False,
+            "balance": tmpl_line.balance or False,
             "python_code": (
                 tmpl_line.python_code if tmpl_line.type == "computed" else False
             ),
